e09daf9002e7d08b833b94ae20bf89b8817577bb
Dylan5019-Odin <dylanm5017@gmail.com>
2021-07-13 20:12:29 -0500

    Added Game Logic: First to 5 wins, displays score, can pick any option

--- test.js
+++ test.js
@@ -49,14 +49,68 @@ const playRound = (playerSelection, computerSelection) => {
     }
 }
 
+let playerScoreText = document.getElementById('playerScore');
+let computerScoreText = document.getElementById('computerScore');
+let resultMessage = document.getElementById('resultText')
+let playerScore = 0;
+let computerScore = 0;
+
 let playGame = function(choice){
     
+
     if (choice == 0) {
-        console.log("rock");
+        let result = playRound('rock', computerPlay());
+        if (result == 1) {
+            playerScore += 1;
+            resultMessage.textContent = "Rock Beats Scissors, Player Wins";
+        } else if (result == 0) {
+            computerScore += 1;
+            resultMessage.textContent = "Paper Beats Rock, Computer Wins";
+        }
+        else {
+            resultMessage.textContent = "Tie Game";
+        }
+       
     } else if (choice == 1 ) {
-        console.log("Paper");
+        let result = playRound('paper', computerPlay());
+        if (result == 1) {
+            playerScore += 1;
+            resultMessage.textContent = "Paper Beats Rock, Player Wins";
+        } else if (result == 0) {
+            computerScore += 1;
+            resultMessage.textContent = "Scissors Beats Paper, Computer Wins";
+        }
+        else {
+            resultMessage.textContent = "Tie Game";
+        }
     }
     else {
-        console.log("Scissors");
+        let result = playRound('scissors', computerPlay());
+        if (result == 1) {
+            playerScore += 1;
+            resultMessage.textContent = "Scissors Beats Paper, Player Wins";
+        } else if (result == 0) {
+            computerScore += 1;
+            resultMessage.textContent = "Rock Beats Scissors, Computer Wins";
+        }
+        else {
+            resultMessage.textContent = "Tie Game";
+        }
     }
+    
+
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+
+    if(playerScore < 5 && computerScore < 5){
+        
+    } else {
+     if(computerScore >= 5) {
+        resultMessage.textContent = "Game Over, The Computer Wins";
+     } else {
+        resultMessage.textContent = "Congrats! You Win! ";
+     }
+ }
+
+
 }
